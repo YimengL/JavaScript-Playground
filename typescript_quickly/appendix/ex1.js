22f5e2f96a234c2b261f5122164f84c5f6d385c9
YimengL <yimengcsu@gmail.com>
2022-11-29 20:57:04 +0100

    Create ex1.js

---
+++ typescript_quickly/appendix/ex1.js
@@ -0,0 +1,6 @@
+var customer = "Joe";
+
+(function () {
+    console.log("The name of the customer inside the function is " + customer);
+})();
+console.log("The name of the customer ourside the function is " + customer);
