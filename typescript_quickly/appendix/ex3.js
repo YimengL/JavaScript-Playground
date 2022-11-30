e48a541c992f1826b99c2d0e2dee0753cb242dd9
Yimeng Li <yimengcsu@gmail.com>
2022-11-30 12:57:37 +0100

    Create ex3.js

---
+++ typescript_quickly/appendix/ex3.js
@@ -0,0 +1,13 @@
+const customerName = "John Smith";
+console.log(`Hello ${customerName}`);
+
+function getCustomer() {
+    return "Allan Lou";
+}
+
+console.log(`Hello ${getCustomer()}`);
+
+const message = `Please enter a password that
+                 has at least 8 characters and
+                 includes a capital letter`;
+console.log(message);
