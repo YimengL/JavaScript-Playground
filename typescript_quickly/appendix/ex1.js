51180f512153a17e6d344f06404854ae5ff6fdc0
Yimeng Li <yimengcsu@gmail.com>
2022-11-30 12:40:35 +0100

    update ex1

--- typescript_quickly/appendix/ex1.js
+++ typescript_quickly/appendix/ex1.js
@@ -2,5 +2,8 @@ var customer = "Joe";
 
 (function () {
     console.log("The name of the customer inside the function is " + customer);
+    if (true) {
+        var customer = "Mary";
+    }
 })();
 console.log("The name of the customer ourside the function is " + customer);
